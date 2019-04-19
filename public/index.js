37d13c71b2e02362a09dad2fc50625abdbdf9fde
David Han <dhansolo@uw.edu>
2019-04-18 20:38:15 -0700

    Fixed random response results, used Zomato's geolocation endpoint to grab more data, adjusted events depending on user's decision to enable location or not

--- public/index.js
+++ public/index.js
@@ -32,29 +32,43 @@ Vue.component('form-comp', {
         }
     },
     mounted () {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            lat = position.coords.latitude;
-            long = position.coords.longitude;
-            console.log(lat, long);
+        console.log(lat, long);
+        axios
+        .get('https://developers.zomato.com/api/v2.1/geocode?lat=' + lat + '&lon=' + long, config)
+        .then(response => {
+            if(response.status == 200) {
+                console.log(response)
+                app.location = response.data.location.city_name;
+            }
         })
+        .catch(error => {
+            console.log(error)
+        });
     },
     methods: {
         onSubmit() {
             app.restaurant_name = null;
             app.address = null;
             app.cuisines = null;
-            app.searched = false;
+            app.results_found = false;
+            app.results_not_found = false;
             let dist = this.distance * 1609.34;
             axios
             .get('https://developers.zomato.com/api/v2.1/search?lat=' + lat + '&lon=' + long + '&radius=' + dist + '&q=' + this.cuisine + '&sort=rating', config)
             .then(response => {
-                console.log('https://developers.zomato.com/api/v2.1/search?lat=' + lat + '&lon=' + long + '&radius=' + dist + '&q=' + this.cuisine + '&sort=rating');
-                let rand = Math.floor(Math.random() * 20);
-                console.log(response.data.restaurants[rand].restaurant);
-                app.restaurant_name = response.data.restaurants[rand].restaurant.name;
-                app.address = response.data.restaurants[rand].restaurant.location.address;
-                app.cuisines = response.data.restaurants[rand].restaurant.cuisines;
-                app.searched = true;
+                let results_length = response.data.restaurants.length;
+                if(results_length > 0) {
+                    // console.log('https://developers.zomato.com/api/v2.1/search?lat=' + lat + '&lon=' + long + '&radius=' + dist + '&q=' + this.cuisine + '&sort=rating');
+                    let rand = Math.floor(Math.random() * response.data.restaurants.length);
+                    // console.log(rand);
+                    // console.log(response.data.restaurants[rand].restaurant);
+                    app.restaurant_name = response.data.restaurants[rand].restaurant.name;
+                    app.address = response.data.restaurants[rand].restaurant.location.address;
+                    app.cuisines = response.data.restaurants[rand].restaurant.cuisines;
+                    app.results_found = true;
+                } else {
+                    app.results_not_found = true;
+                }
             })
             .catch(error => {
                 console.log(error)
@@ -66,9 +80,23 @@ Vue.component('form-comp', {
 var app = new Vue({
     el: '#app',
     data: {
-        restaurant_name: 'hello',
+        location: null,
+        restaurant_name: null,
         address: null,
         cuisines: null,
-        searched: false,
+        results_found: false,
+        results_not_found : false,
+        location_found: false
+    },
+    mounted () {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+            app.location_found = true;
+        }, function(err) {
+            if(err.code == err.PERMISSION_DENIED) {
+                console.log('please enable location services');
+            }
+        })
     }
 })
