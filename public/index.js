b1c5ace1a4173dfd4da78c93f6753edc3f45077e
David Han <dhansolo@uw.edu>
2019-04-21 11:25:06 -0700

    Small changes to help with readability

--- public/index.js
+++ public/index.js
@@ -4,8 +4,7 @@ const config = {
     }
 };
 
-
-
+const map_key = "";
 let lat = null;
 let long = null;
 
@@ -30,13 +29,13 @@ Vue.component('form-comp', {
         }
     },
     mounted () {
-        console.log(lat, long);
+        // console.log(lat, long);
         axios
-        .get('https://developers.zomato.com/api/v2.1/geocode?lat=' + lat + '&lon=' + long, config)
+        .get('https://developers.zomato.com/api/v2.1/cities?lat=' + lat + '&lon=' + long, config)
         .then(response => {
             if(response.status == 200) {
-                console.log(response)
-                app.location = response.data.location.city_name;
+                // console.log(response)
+                app.location = response.data.location_suggestions[0].name + ", " + response.data.location_suggestions[0].country_name;
             }
         })
         .catch(error => {
@@ -52,27 +51,25 @@ Vue.component('form-comp', {
             app.map = null;
             app.results_found = false;
             app.results_not_found = false;
-            let dist = this.distance * 1609.34;
+            let meters = this.distance * 1609.34; // Meters to Miles
             axios
-            .get('https://developers.zomato.com/api/v2.1/search?lat=' + lat + '&lon=' + long + '&radius=' + dist + '&q=' + this.cuisine + '&sort=rating', config)
+            .get('https://developers.zomato.com/api/v2.1/search?lat=' + lat + '&lon=' + long + '&radius=' + meters + '&q=' + this.cuisine + '&sort=rating', config)
             .then(response => {
                 let results_length = response.data.restaurants.length;
                 if(results_length > 0) {
                     // console.log(response);
-                    // console.log('https://developers.zomato.com/api/v2.1/search?lat=' + lat + '&lon=' + long + '&radius=' + dist + '&q=' + this.cuisine + '&sort=rating');
                     let rand = Math.floor(Math.random() * response.data.restaurants.length);
+                    let payload = response.data.restaurants[rand].restaurant;
                     // console.log(rand);
                     // console.log(response.data.restaurants[rand].restaurant);
-                    app.restaurant_name = response.data.restaurants[rand].restaurant.name;
-                    app.address = response.data.restaurants[rand].restaurant.location.address;
-                    app.cuisines = response.data.restaurants[rand].restaurant.cuisines;
-                    app.link = response.data.restaurants[rand].restaurant.url;
-                    let restaurant_lat = response.data.restaurants[rand].restaurant.location.latitude;
-                    let restaurant_long = response.data.restaurants[rand].restaurant.location.longitude;
+                    app.restaurant_name = payload.name;
+                    app.address = payload.location.address;
+                    app.cuisines = payload.cuisines;
+                    app.link = payload.url;
+                    let restaurant_lat = payload.location.latitude;
+                    let restaurant_long = payload.location.longitude;
                     let updated_rest_name = app.restaurant_name.replace(/[^a-zA-Z ]/g, "").split(' ').join('+');
-                    console.log(updated_rest_name);
-                    app.map = "https://www.google.com/maps/embed/v1/search?key=&q=" + updated_rest_name + "&center=" + restaurant_lat + "," + restaurant_long + "&maptype=roadmap&zoom=15";
-                    console.log(app.map);
+                    app.map = "https://www.google.com/maps/embed/v1/search?key=" + map_key + "&q=" + updated_rest_name + "&center=" + restaurant_lat + "," + restaurant_long + "&maptype=roadmap&zoom=15";
                     app.results_found = true;
                 } else {
                     app.results_not_found = true;
